384949563e5443cc57ac27d0b46eaaed192851e6
Veljko <kanjevacveljko@gmail.com>
2025-09-09 22:15:42 +0200

    v6-zadaci4-5

--- VEZBA_6.sql
+++ VEZBA_6.sql
@@ -5,7 +5,7 @@
 --a) Kreirati funkciju fun_Posao, koja za zadato ime i prezime radnika vraća posao,
 --kvalifikaciju i primanja.
 
-create function fun_Posao 
+create function fun_Posao
 (@ime nchar(20),
 @prezime nchar(20))
 returns table
@@ -87,12 +87,27 @@ where posao = N'analitičar';
 --Kreirati funkciju fun_RadnikOdeljenje1 koja u zavisnosti od prosleđenog mesta odeljenja
 --prikazuje ime, prezime i posao radnika kao i ime odeljenja u kome radi.
 
-
+create function fun_RadnikOdeljenje1(@mesto nchar(30))
+returns @Rezultat table 
+		(ime nchar(20),
+		prezime nchar(20),
+		posao nchar(20),
+		ime_od nchar(20))
+as
+begin
+	insert into @Rezultat
+	select r.ime, r.prezime, r.posao, o.ime_od
+	from radnik r inner join odeljenje o
+		 on r.Id_odeljenja = o.Id_odeljenja
+	where mesto = @mesto
+return
+end;
 
 --b)Prikazati ime, prezime, posao i naziv odeljenja za radnike koji rade na Dorćolu.
 --Koristiti funkciju fun_RadnikOdeljenje1.
 
-
+select * 
+from dbo.fun_RadnikOdeljenje1(N'Dorćol');
 
 --Zadatak 5:
 
@@ -100,7 +115,27 @@ where posao = N'analitičar';
 --identifikacionog broja profesora prikazuje ime i prezime profesora i nazive predmeta koje
 --predaje. Uraditi zadatak korišćenjem MSTVF.
 
+use STUDENTSKA
+go
+
+create function fun_ProfesorPredmet(@idProf int)
+returns @Rezultat table
+		(ime nchar(30),
+		prezime nchar(30),
+		naziv nchar(50))
+as
+begin
+	insert into @Rezultat
+	select ime, prezime, naziv
+	from profesor inner join predmet
+		 on profesor.Id_profesora = predmet.id_profesora
+	where profesor.id_profesora = @idProf
+return
+end;
 
 
 --b) Prikazati ime, prezime profesora i nazive predmeta koje predaje za profesora čiji je
 --identifikacioni broj 106.
+
+select *
+from dbo.fun_ProfesorPredmet(106);
