18a4851340d28d2522d1400071bb4904c04f6b57
Veljko <kanjevacveljko@gmail.com>
2025-09-09 19:04:46 +0200

    v4-svi zadaci

--- VEZBA_4.sql
+++ VEZBA_4.sql
@@ -5,28 +5,51 @@
 --a) Kreirati pogled View_Penzioneri koji prikazuje imena i prezimena radnika koji imaju više
 --od 40 godina staža.
 
-
+create view View_Penzioneri 
+as
+select ime, prezime
+from radnik
+where datediff(yy, dat_zap, getdate()) >= 40;
 
 --b) Prikazati imena i prezimena radnika koji imaju više od 40 godina staža.
 
+select * from View_Penzioneri;
 
-
---Zadatak 2: Kreirati pogled View_Sefov_Projekti koji prikazuje imena i prezime šefova odeljenja i
+--Zadatak 2: Kreirati pogled View_Sefovi_Projekti koji prikazuje imena i prezime šefova odeljenja i
 --broj projekata na kojima rade.
 
-
+create view View_Sefovi_Projekti
+as
+select ime, prezime, count(ucesce.id_projekta) as broj_projekata
+from ucesce, radnik
+where ucesce.Id_radnika = radnik.Id_radnika
+	  and radnik.id_radnika in (select sef_odeljenja
+								from odeljenje)
+group by radnik.Id_radnika, ime, prezime;
 
 --Zadatak 3:
 
 --a) Kreirati pogled View_Rukovodioci_Sefovi koji prikazuje imena i prezimena rukovodilaca,
 --kao imena i prezimena šefova odeljenja (duplo prikazati ako je radnik i rukovodilac i šef).
 
-
+create view View_Rukovodioci_Sefovi
+as
+select ime, prezime
+from radnik
+where radnik.id_radnika in (select sef_odeljenja 
+							from odeljenje)
+union all
+select ime, prezime
+from radnik
+where radnik.id_radnika in (select id_radnika 
+							from ucesce where funkcija = N'šef');
 
 --b) Pomoću kreirang pogleda View_Rukovodioci_Sefovi prikazati ime i prezime radnika
 --i broj koji prikazuje koliko rukovodećih pozicija (šef, rukovodilac) ima radnik.
 
-
+select ime, prezime, count(*) as broj
+from View_Rukovodioci_Sefovi
+group by ime, prezime;
 
 --4.2 POGLEDI – ALTER VIEW I DROP VIEW
 
