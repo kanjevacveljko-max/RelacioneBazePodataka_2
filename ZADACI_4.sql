700c49cf18ad1bd026ae45116d60f8c9b1bee319
Veljko <kanjevacveljko@gmail.com>
2025-09-10 05:16:02 +0200

    z4-svi zadaci

--- ZADACI_4.sql
+++ ZADACI_4.sql
@@ -1,18 +1,32 @@
---1. Kreirati pogled View_Student_Prosek koji prikazuje indeks, ime, prezime i prose?nu ocenu
+--1. Kreirati pogled View_Student_Prosek koji prikazuje indeks, ime, prezime i prosecnu ocenui smer
 --svih studenata.
 
+create view View_Student_Prosek
+as
+select concat(s.smer,'-',cast(s.broj as nvarchar),'/',substring(s.godina_upisa,3,2)) as indeks,
+	   s.ime, s.prezime, s.smer, round(avg(ocena),2) as prosecna_ocena
+from student s, zapisnik z
+where s.id_studenta = z.id_studenta
+group by z.id_studenta, s.ime, s.prezime, s.broj, s.smer, s.godina_upisa
 
 
---2. Izmeniti pogled View_Student_Prosek tako da prikazuje i smer studenta.
+--2. Koristeci pogled View_Student_Prosek prikazati prosecne ocene studenata RT smera.
 
+select *
+from View_Student_Prosek
+where smer = 'rt'
 
+--3. Dodati enkripciju pogledu View_Student_Prosek.
 
---3. Koriste?i pogled View_Student_Prosek prikazati prose?ne ocene studenata RT smera.
+alter view View_Student_Prosek
+with encryption
+as
+select concat(s.smer,'-',cast(s.broj as nvarchar),'/',substring(s.godina_upisa,3,2)) as indeks,
+	   s.ime, s.prezime, s.smer, round(avg(ocena),2) as prosecna_ocena
+from student s, zapisnik z
+where s.id_studenta = z.id_studenta
+group by z.id_studenta, s.ime, s.prezime, s.broj, s.smer, s.godina_upisa
 
+--4. Izbrisati pogled View_Student_Prosek
 
-
---4. Dodati enkripciju pogledu View_Student_Prosek.
-
-
-
---5. Izbrisati pogled View_Student_Prosek
+drop view View_Student_Prosek;
