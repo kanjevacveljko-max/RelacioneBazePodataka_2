5b1e8eceaad216d0f4c9d86289de2ec573439791
Veljko <kanjevacveljko@gmail.com>
2025-09-16 19:40:28 +0200

    commit

--- VEZBA_6.sql
+++ VEZBA_6.sql
@@ -117,6 +117,23 @@ from dbo.fun_RadnikOdeljenje1(N'Dorćol');
 --identifikacionog broja profesora prikazuje ime i prezime profesora i nazive predmeta koje
 --predaje. Uraditi zadatak korišćenjem MSTVF.
 
+use STUDENTSKA
+go
+
+create function fun_ProfesorPredmet(@idProf int)
+returns @Rezultat table
+		(ime nchar(30),
+		prezime nchar(30),
+		naziv nchar(50))
+as
+begin
+	insert into @Rezultat
+	select ime, prezime, naziv
+	from profesor inner join predmet
+		 on profesor.Id_profesora = predmet.id_profesora
+	where profesor.id_profesora = @idProf
+return
+end;
 
 
 --b) Prikazati ime, prezime profesora i nazive predmeta koje predaje za profesora čiji je
